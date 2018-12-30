2d76726dcc5ee7178357271db853213db5836909
evgenii-codesmith <evgenii.codesmith@gmail.com>
2018-12-30 01:34:43 -0800

    Jest initial tests

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   presets: [
-    '@vue/app'
-  ]
+    '@vue/app',
+    ["env", { "modules": false }]
+  ],
+  "env": {
+    "test": {
+      "presets": [
+        ["env", { "targets": { "node": "current" }}]
+      ]
+    }
+  }
 };
